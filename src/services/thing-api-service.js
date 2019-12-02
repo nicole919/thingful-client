acd303f97df2a0bb88ca2d88421f8d55d30f6f9c
Nicole Fafard <nicole.fafard@gmail.com>
2019-12-02 11:51:21 -0700

    update secure login

--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -4,21 +4,21 @@ import config from "../config";
 const ThingApiService = {
   getThings() {
     return fetch(`${config.API_ENDPOINT}/things`, {
-      headers: { authorization: `basic ${TokenService.getAuthToken()}` }
+      headers: { authorization: `bearer ${TokenService.getAuthToken()}` }
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
   },
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
-      headers: { authorization: `basic ${TokenService.getAuthToken()}` }
+      headers: { authorization: `bearer ${TokenService.getAuthToken()}` }
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
   },
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
-      headers: { authorization: `basic ${TokenService.getAuthToken()}` }
+      headers: { authorization: `bearer ${TokenService.getAuthToken()}` }
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
